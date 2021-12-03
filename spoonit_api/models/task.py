1d6ff01303394574283676e77605e284597d9baa
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-03 11:05:51 -0600

    corrected some typos

--- spoonit_api/models/task.py
+++ spoonit_api/models/task.py
@@ -1,14 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from spoonit_api.models import Category, Spoon, TaskList
-
 
 class Task(models.Model):
     name = models.CharField(max_length=60)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    spoon = models.ForeignKey(Spoon, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    spoon = models.ForeignKey('Spoon', on_delete=models.CASCADE)
     description = models.TextField()
-    created_by = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     is_universal = models.BooleanField()
-    spoonies = models.ManyToManyField(User, through=TaskList, related_name='tasks')
+    spoonies = models.ManyToManyField(User, through='TaskList', related_name='tasks')

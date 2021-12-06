2a726cce3bd81891d77186f4f2953b3dc93cdded
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-06 11:24:12 -0600

    added urls

--- spoonit/urls.py
+++ spoonit/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.conf.urls import include
 from spoonit_api.views import register_user, login_user
 from django.urls import path
+from rest_framework import routers
+from spoonit_api.views import TaskView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('tasks', TaskView, 'task')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),

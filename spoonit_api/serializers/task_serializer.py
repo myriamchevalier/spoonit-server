3ab12e41bb0b3e87b8de4bd88574e1bd84f2e146
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-06 11:21:04 -0600

    added serializer and view for tasks, defined list function

---
+++ spoonit_api/serializers/task_serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from spoonit_api.models import Task
+
+
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('id', 'name', 'category', 'spoon', 'description', 'created_by', 'is_universal', 'is_active')
+        depth = 1
+
+
+
--- spoonit_api/views/__init__.py
+++ spoonit_api/views/__init__.py
@@ -1,1 +1,2 @@
 from .auth import register_user, login_user
+from .task_view import TaskView

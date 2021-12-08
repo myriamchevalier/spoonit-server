f91588013d04bdc6e499d290f6660169a84a08f6
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-08 11:43:26 -0600

    added an action method allowing user to get one random task

--- spoonit_api/views/__init__.py
+++ spoonit_api/views/__init__.py
@@ -2,3 +2,4 @@ from .auth import register_user, login_user
 from .task_view import TaskView
 from .category_view import CategoryView
 from .spoon_view import SpoonView
+# from .random_task_view import RandomTaskView

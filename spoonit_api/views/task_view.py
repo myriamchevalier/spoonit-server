94ee5a561f3ef638c6603940dbc035672eb4434e
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-09 10:04:05 -0600

    added filter options to randomizer function

--- spoonit_api/views/task_view.py
+++ spoonit_api/views/task_view.py
@@ -82,7 +82,23 @@ class TaskView(ViewSet):
 
     @action (methods=['get'], detail=False)
     def random_task(self, request):
-        random_task = Task.objects.order_by('?')[0]
+        
+        tasks = Task.objects.all()
+        created_by = request.query_params.get('created_by', None)
+        category = request.query_params.get('category', None)
+        spoon = request.query_params.get('spoon', None)
+        
+        if created_by is not None:
+            tasks = tasks.filter(created_by__id=request.auth.user.id)
+
+        if spoon is not None:
+            # to filter through all the tasks, must filter through the result of first query.
+            tasks = tasks.filter(spoon__id=spoon)
+
+        if category is not None:
+            tasks = tasks.filter(category__id=category)
+        
+        random_task = tasks.order_by('?')[0]
 
         serializer = TaskSerializer(random_task)
         return Response (serializer.data)

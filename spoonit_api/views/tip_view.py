fcbf8cdab6ed403c73dfce37b017dea75580c0f0
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-10 14:23:27 -0600

    added functions to achieve CRUD on tip_view

--- spoonit_api/views/tip_view.py
+++ spoonit_api/views/tip_view.py
@@ -1,7 +1,8 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from spoonit_api.models import Tip
+from spoonit_api.models import Tip, Topic
 from spoonit_api.serializers import TipSerializer
 
 class TipView(ViewSet):
@@ -20,3 +21,63 @@ class TipView(ViewSet):
         
         serializer = TipSerializer(tips, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST requests to create new tips
+        
+        Returns
+            Response -- JSON serialized tip instance
+        """
+
+        spoonie = request.auth.user
+        topic = Topic.objects.get(pk=request.data['topicId'])
+
+        try:
+            tip = Tip.objects.create(
+                topic = topic,
+                content = request.data['content'],
+                spoonie = spoonie
+            )
+            serializer = TipSerializer(tip)
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({'reason': ex.message}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        """Handle PUT requests for updating a single tip
+        
+        Returns:
+            Response -- 204 , 404 or 500 status code
+        """
+        user_tip = Tip.objects.filter(spoonie__id=request.auth.user.id)
+        tip = user_tip.get(pk=pk)
+
+        try :
+            tip.topic = Topic.objects.get(pk=request.data['TopicId'])
+            tip.content = request.data['content']
+            tip.spoonie = request.auth.user
+            tip.save()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Tip.DoesNotExist as ex:
+            return Response({'message' : ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)        
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single tip
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            user_tip = Tip.objects.filter(spoonie__id=request.auth.user.id)
+            tip = user_tip.get(pk=pk)
+            tip.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Tip.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
